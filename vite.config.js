fea723b59ba585c874817a9fe499d1580c8bddf7
Maksym Kapula <maksym.kapula@gmail.com>
2026-02-27 11:41:54 +0200

    base: '/Watching-sculptures-V.K./', // важливо з косими рисками і крапкою

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Watching-sculptures-V.K./',
+  base: '/Watching-sculptures-V.K./', // важливо з косими рисками і крапкою
 })
